47d5a38c7a3e549e6b2309534b8d6c05ceffff4b
hutian <1277769594@qq.com>
2019-01-25 18:06:00 +0800

    xiaofeiniu-api 服务器搭建

---
+++ index.js
@@ -0,0 +1,9 @@
+// 小肥牛扫码点餐API子系统
+const PORT = 8090;
+const express = require('express');
+
+// 启动主服务器
+var app = express();
+app.listen(PORT,()=>{
+    console.log("Server Listening" +PORT+'...');
+})
