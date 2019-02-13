deddd7529992e809b2b79ed0b43451b0a6a05a74
hutian <1277769594@qq.com>
2019-02-13 14:18:45 +0800

    添加必需API路由

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ app.use(cors());
 //app.use(bodyParser.urlencoded({}))  //把application/x-www-form-urlencoded格式的请求主体数据解析出来放入req.body属性
 app.use(bodyParser.json()); //把application/json格式的请求主体数据解析出来放入req.body属性
 
-//挂载路由器
+//挂载管理后台必需路由器
 app.use('/admin/category', categoryRouter);
 app.use('/admin', adminRouter);
 app.use('/admin/dish', dishRouter);
 app.use('/admin/settings',settingsRouter)
 app.use('/admin/table',tableRouter)
+
+//挂载顾客app必需的路由器
+// app.use('/dish',dishRouter)
